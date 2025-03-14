6e04c9a8a0b849635e6787fa87c896dc22c0add3
KTH-ARYAN <aryan@kriscent.in>
2025-03-15 02:39:58 +0530

    vcxv

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors({
       origin: [
         "http://localhost:5173",
-        "https://ed-tech-backend-daig.onrender.com"
+        "https://ed-tech-frontend-y3sn.vercel.app"
       ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
